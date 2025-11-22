3d41770df51f4d2e6bbb84159e67f647aaa2e041
Romit Sagu <romit.sagu@hotmail.com>
2025-11-22 00:20:34 -0500

    Use Poppins font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/theme-provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "PrismAuth - OAuth2 Authentication",
@@ -31,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.className} antialiased`}
       >
         <ThemeProvider defaultTheme="system" storageKey="prismauth-theme">
           {children}
